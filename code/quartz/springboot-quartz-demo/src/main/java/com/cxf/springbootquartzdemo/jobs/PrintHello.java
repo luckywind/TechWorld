bc86fc721b7edf496912a7a43e6de716c41291e5
chengxingfu <1318247907@qq.com>
2020-05-23 12:44:49 +0800

    add DisallowConcurrentExecution

--- code/quartz/springboot-quartz-demo/src/main/java/com/cxf/springbootquartzdemo/jobs/PrintHello.java
+++ code/quartz/springboot-quartz-demo/src/main/java/com/cxf/springbootquartzdemo/jobs/PrintHello.java
@@ -1,5 +1,6 @@
 package com.cxf.springbootquartzdemo.jobs;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -11,6 +12,13 @@ import java.time.LocalDateTime;
  * Authors: chengxingfu <chengxingfu@xiaomi.com>
  * Date:2020-05-23
  */
+
+/**
+ * Job 的实例要到该执行它们的时候才会实例化出来。每次 Job 被执行，一个新的 Job 实例会被创建。
+ * 其中暗含的意思就是你的 Job 不必担心线程安全性，因为同一时刻仅有一个线程去执行给定 Job 类的实例，甚至是并发执行同一 Job 也是如此。
+ * @DisallowConcurrentExecution 保证上一个任务执行完后，再去执行下一个任务
+ */
+@DisallowConcurrentExecution
 public class PrintHello implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
