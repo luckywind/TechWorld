5ed8f67187befb909077dcc222deb456dc65020d
chengxingfu <1318247907@qq.com>
2020-06-08 22:49:23 +0800

    父子组件

--- code/web/angular/angular-tour-of-heroes/src/app/app.module.ts
+++ code/web/angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- code/web/angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ code/web/angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+  // 这个用@Input注解的hero属性，使得外部的HeroesComponent组件可以绑定它，例如：
+  // <app-hero-detail [hero]="selectedHero"></app-hero-detail>
+  @Input() hero: Hero;
   constructor() { }
 
   ngOnInit(): void {
