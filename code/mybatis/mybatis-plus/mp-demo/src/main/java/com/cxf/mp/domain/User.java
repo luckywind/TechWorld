d7f6d0c3b3cbca0ac385fe8190cdc97a6705a91c
chengxingfu <1318247907@qq.com>
2020-08-05 00:19:38 +0800

    完成自动填充

---
+++ code/mybatis/mybatis-plus/mp-demo/src/main/java/com/cxf/mp/domain/User.java
@@ -0,0 +1,49 @@
+package com.cxf.mp.domain;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.Data;
+
+/** 
+* Copyright (c) 2015 XiaoMi Inc. All Rights Reserved. 
+* Authors: chengxingfu <chengxingfu@xiaomi.com>
+* Date:2020-08-05  
+*/
+@ApiModel(value="com-cxf-mp-domain-User")
+@Data
+@TableName(value = "`user`")
+public class User {
+    @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value="")
+    private Integer id;
+
+    @TableField(value = "username")
+    @ApiModelProperty(value="")
+    private String username;
+
+    @TableField(value = "password")
+    @ApiModelProperty(value="")
+    private String password;
+
+    /**
+     * 逻辑删除
+     */
+    @TableField(value = "deleted",select = false)
+    @ApiModelProperty(value="逻辑删除")
+    private Integer deleted;
+
+    @TableField(value = "create_time",fill = FieldFill.INSERT)
+    @ApiModelProperty(value="")
+    private LocalDateTime createTime;
+
+    @TableField(value = "update_time",fill = FieldFill.UPDATE)
+    @ApiModelProperty(value="")
+    private LocalDateTime updateTime;
+}
